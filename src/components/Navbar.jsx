90b540d11fef2a8a05120bd468b12f029961851d
apjavier2 <apjavier27@gmail.com>
2023-03-23 14:18:28 +0800

    added ease-in -out in navBar hidden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         className={
           !nav
             ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT</h1>
